3d923461d9abda7c7f22cda3653d6b3d4f7bd939
G40styBoy <81126701+G40styBoy@users.noreply.github.com>
2024-03-15 14:44:00 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static int linesMade;
+
+
+    public static byte[] Name_Current = { 0x43, 0x75, 0x72, 0x72, 0x65, 0x6E, 0x74, 0x00 };  // Current
+    public static byte[] Name_TotalAcquired = { 0x54, 0x6F, 0x74, 0x61, 0x6C, 0x41, 0x63, 0x71, 0x75, 0x69, 0x72, 0x65, 0x64, 0x00 };
+    public static byte[] Name_TotalFoundInWorld = { 0x54, 0x6F, 0x74, 0x61, 0x6C, 0x46, 0x6F, 0x75, 0x6E, 0x64, 0x49, 0x6E, 0x57, 0x6F, 0x72, 0x6C, 0x64, 0x00 };
+    public static byte[] Name_TotalFromBattle = { 0x54, 0x6F, 0x74, 0x61, 0x6C, 0x46, 0x72, 0x6F, 0x6D, 0x42, 0x61, 0x74, 0x74, 0x6C, 0x65, 0x00 };
+
+    static string relativePath = @".\SAVE\UnencryptedSave0.bin";
+    static string saveFilePath = Path.GetFullPath(relativePath);
+
+    public class CurrencyStruct
+    {
+        public int Current { get; set; }
+        public int TotalAcquired { get; set; }
+        public int TotalFoundInWorld { get; set; }
+        public int TotalFromBattle { get; set; }
+    }
+
+    static void Main()
+    {
+
+        bool readF = File.Exists(saveFilePath);
+
+        if (!readF)
+        {
+            Console.WriteLine("File not found. Press any key to exit.");
+            Console.ReadKey();
+            return;
+        }
+
+        using (BinaryReader reader = new BinaryReader(File.Open(saveFilePath, FileMode.Open)))
+        {
+            Stream stream = reader.BaseStream;  // Get the underlying stream
+            BinaryWriter writer = new BinaryWriter(stream);  // Create writer instance
+
+
+            while (true)
+            {
+                bool returnSetup = setupLines();
+
+                if (!returnSetup)
+                {
+                    Console.ReadKey();
+                    return;
+                }
+
+                writeLine("1. Edit Save File");
+                writeLine("2. Exit");
+
+                Console.Write("Enter your choice: ");
+                linesMade++;
+
+                var choice = Console.ReadLine();
+
+                ClearConsoleLines(linesMade);
+
+                switch (choice)
+                {
+                    case "1":
+                        EditSaveFile(reader, writer);
+                        break;
+                    case "2":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+                //Console.ReadLine();
+            }
+        }
+    }
+
+    static void EditSaveFile(BinaryReader reader, BinaryWriter writer)
+    {
+        while (true)
+        {
+
+            writeLine("1. Edit Currency");
+            writeLine("2. Return Home");
+
+            Console.Write("Enter your choice: ");
+            linesMade++;
+
+            var editChoice = Console.ReadLine();
+
+            ClearConsoleLines(linesMade);
+
+            switch (editChoice)
+            {
+                case "1":
+                    EditCurrency(reader, writer);
+                    break;
+                case "2":
+                    return;
+                default:
+                    writeLine("Invalid choice. Please try again.");
+                    Console.ReadKey();
+                    ClearConsoleLines(linesMade);
+                    break;
+            }
+
+            //Console.ReadLine();
+        }
+    }
+
+    static void InfoSection()
+    {
+        Console.Write("Not implemented yet.");
+        Console.ReadKey();
+    }
+
+    static void EditCurrency(BinaryReader reader, BinaryWriter writer)
+    {
+        // goldCurrent = findGold
+        // chipsCurrent = findChips
+        Util util = new Util();
+        bool buff1 = false;
+        bool buff2 = false;
+
+        if (reader.BaseStream.Position != 0)  // reset stream position
+        {
+            ResetStreamPosition(reader);
+        }
+
+        CurrencyStruct goldStruct = new CurrencyStruct
+        {
+            Current = util.FindAndReadValue(reader, Name_Current),
+            TotalAcquired = util.FindAndReadValue(reader, Name_TotalAcquired),
+            TotalFoundInWorld = util.FindAndReadValue(reader, Name_TotalFoundInWorld),
+            TotalFromBattle = util.FindAndReadValue(reader, Name_TotalFromBattle)
+        };
+
+        ResetStructFind();
+
+        CurrencyStruct chipsStruct = new CurrencyStruct
+        {
+            Current = util.FindAndReadValue(reader, Name_Current),
+            TotalAcquired = util.FindAndReadValue(reader, Name_TotalAcquired),
+            TotalFoundInWorld = 0,
+            TotalFromBattle = 0
+        };
+
+
+        writeLine("Gold Stats");
+        writeLine("Amount: " + goldStruct.Current);
+        writeLine("Total Acquired: " + goldStruct.TotalAcquired);
+        writeLine("Total Found In World: " + goldStruct.TotalFoundInWorld);
+        writeLine("Total From Battle: " + goldStruct.TotalFromBattle);
+        writeLine("");
+        writeLine("Chips Stats");
+        writeLine("Amount: " + chipsStruct.Current);
+        writeLine("Total Acquired: " + chipsStruct.TotalAcquired);
+        writeLine("");
+
+        ResetStreamPosition(reader);
+
+        Console.Write("New Gold Value: ");
+        linesMade++;
+        var goldNew = Console.ReadLine();
+        ClearConsoleLines(1);
+
+        Console.Write("New Chips Value: ");
+        linesMade++;
+        var chipsNew = Console.ReadLine();
+        ClearConsoleLines(1);
+
+        bool goldSuc = long.TryParse(goldNew, out long num1);
+        bool chipSuc = long.TryParse(chipsNew, out long num2);
+
+        try
+        {
+            if (!string.IsNullOrEmpty(goldNew) && !string.IsNullOrEmpty(chipsNew))
+            {
+                buff1 = Util.FindAndWriteValue(reader, writer, Name_Current, ConvertStringToInt(goldNew));
+                ResetStructFind();
+                buff2 = Util.FindAndWriteValue(reader, writer, Name_Current, ConvertStringToInt(chipsNew));
+                FlushStream(writer);  // flush changes
+            }
+            if (buff1 && buff2)
+            {
+                ClearConsoleLines(linesMade);
+                writeLine("Edits made! Press any key to continue...");
+            }
+            else
+            {
+                ClearConsoleLines(linesMade);
+                writeLine("Something went wrong!");
+                if (!buff1 || !buff2)
+                {
+                    writeLine("Gold returned: " + buff1);
+                    writeLine("Chips returned: " + buff2);
+                }
+            }
+
+        }
+
+        catch (OverflowException)
+        {
+            writeLine("Number too big!");
+        }
+
+        Console.ReadKey();
+        ClearConsoleLines(linesMade);
+    }
+
+    static void ClearConsoleLines(int numLines)
+    {
+        if (numLines <= 0)
+            return;
+
+        var currentLineCursor = Console.CursorTop;
+        var newLineCursor = currentLineCursor - numLines;
+        newLineCursor = newLineCursor < 0 ? 0 : newLineCursor;
+
+        Console.SetCursorPosition(0, newLineCursor);
+        for (int i = 0; i < numLines; i++)
+        {
+            Console.Write(new string(' ', Console.BufferWidth));
+            linesMade--;
+        }
+        Console.SetCursorPosition(0, newLineCursor);
+    }
+
+
+    static void writeLine(string str)
+    {
+        Console.WriteLine(str);
+        linesMade++;
+    }
+
+    static bool setupLines()
+    {
+        string foundFile = "";
+
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.WriteLine("Infinity Blade 3 Save Editor");
+        Console.ResetColor();
+        Console.Write("Save File Loaded: ");
+        if (File.Exists(saveFilePath))
+        {
+            foundFile = "True";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(foundFile + "\n\n");
+        }
+        else
+        {
+            foundFile = "False";
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(foundFile + "\n\n");
+            Console.ResetColor();
+            Console.Write("Make sure the save file is located inside of the file \"SAVE\" so the program can locate it, and that its named \"UnencryptedSave0\".");
+            Console.WriteLine("\n\nPress any key to exit program...");
+            return false;  // file not found
+        }
+
+        Console.ResetColor();
+        return true;  // file found
+    }
+
+    static void ResetStreamPosition(BinaryReader reader)
+    {
+        reader.BaseStream.Position = 0;
+    }
+
+    static void ResetStructFind()
+    {
+        Util.StructFound = false;
+    }
+
+    static int ConvertStringToInt(string str)
+    {
+        int result;
+        if (int.TryParse(str, out result))
+        {
+            return result;
+        }
+        else
+        {
+            throw new FormatException("The string cannot be converted to an integer.");
+        }
+    }
+
+    static void FlushStream(BinaryWriter writer)
+    {
+        writer.Flush();
+    }
+
+}
---
+++ Util.cs
@@ -0,0 +1,272 @@
+﻿using System.Text.Json;
+using System.IO;
+using System.Text;
+
+
+public class Util
+{
+
+    //---------------------------------------------------------------------Special Properties---------------------------------------------------------------------//
+
+    public static byte[] Name_CurrencyStruct = { 0x43, 0x75, 0x72, 0x72, 0x65, 0x6E, 0x63, 0x79, 0x53, 0x74, 0x72, 0x75, 0x63, 0x74, 0x00 };  // Currency Struct
+    public static byte[] Name_Current = { 0x43, 0x75, 0x72, 0x72, 0x65, 0x6E, 0x74, 0x00 };  // Current
+    public static byte[] Name_TotalAcquired = { 0x54, 0x6F, 0x74, 0x61, 0x6C, 0x41, 0x63, 0x71, 0x75, 0x69, 0x72, 0x65, 0x64, 0x00 };
+    public static byte[] Name_TotalFoundInWorld = { 0x54, 0x6F, 0x74, 0x61, 0x6C, 0x46, 0x6F, 0x75, 0x6E, 0x64, 0x49, 0x6E, 0x57, 0x6F, 0x72, 0x6C, 0x64, 0x00 };
+    public static byte[] Name_TotalFromBattle = { 0x54, 0x6F, 0x74, 0x61, 0x6C, 0x46, 0x72, 0x6F, 0x6D, 0x42, 0x61, 0x74, 0x74, 0x6C, 0x65, 0x00 };
+
+    //---------------------------------------------------------------------Uniform Properties---------------------------------------------------------------------//
+
+
+    public static byte[] IntProperty = { 0x49, 0x6E, 0x74, 0x50, 0x72, 0x6F, 0x70, 0x65, 0x72, 0x74, 0x79, 0x00 };  // IntProperty
+    public static byte[] valueSize_Integer = { 0x04, 0x00, 0x00, 0x00 };
+    public static byte[] valueByteArray = { 0x00, 0x00, 0x00, 0x00 };
+
+
+    public static bool StructFound;
+    //---------------------------------------------------------------------Util Functions---------------------------------------------------------------------//
+
+    public static byte[] IntToLittleEndian(int number)
+    {
+        byte[] bytes = BitConverter.GetBytes(number);
+        if (BitConverter.IsLittleEndian)
+            return bytes;
+        else
+            return bytes.Reverse().ToArray();
+    }
+
+    public static int LittleEndianToInt(byte[] bytes)
+    {
+        if (BitConverter.IsLittleEndian)
+            return BitConverter.ToInt32(bytes, 0);
+        else
+            return BitConverter.ToInt32(bytes.Reverse().ToArray(), 0);
+    }
+
+    public static void PrintBytes(byte[] bytes)
+    {
+        foreach (byte b in bytes)
+        {
+            Console.Write("0x{0:X2} ", b);
+        }
+        Console.WriteLine();
+    }
+
+    public static void ClearConsoleLines(int clearCount)
+    {
+        Console.SetCursorPosition(0, Console.CursorTop - clearCount);
+        for (int i = 0; i < clearCount; i++)
+        {
+            Console.Write(new string(' ', Console.BufferWidth));
+        }
+        Console.SetCursorPosition(0, Console.CursorTop - clearCount);
+    }
+
+    public static void ConvertStrBytesToCSVariable(string str, string varName)
+    {
+        string[] hexValuesSplit = str.Split(' ');
+        byte[] byteArray = new byte[hexValuesSplit.Length];
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < hexValuesSplit.Length; i++)
+        {
+            byteArray[i] = Convert.ToByte(hexValuesSplit[i], 16);
+        }
+
+        foreach (byte b in byteArray)
+        {
+            sb.Append("0x");
+            sb.Append(b.ToString("X2"));
+            sb.Append(", ");
+        }
+
+        string result = sb.ToString().TrimEnd(',', ' ');
+
+        if (varName != "")
+        {
+            Console.WriteLine("public static byte[] " + varName + " = {" + result + "};");
+        }
+
+
+    }
+
+
+
+
+
+    public static void LocateStructForBinaryReader(BinaryReader reader, byte[] structName)
+    {
+        int matchIndex = 0;
+
+        while (!StructFound && matchIndex < structName.Length && reader.BaseStream.Position < reader.BaseStream.Length)
+        {
+            byte b = reader.ReadByte();
+            if (b == structName[matchIndex])
+            {
+                matchIndex++;
+                if (matchIndex == structName.Length)
+                {
+                    StructFound = true;  // struct located
+                }
+            }
+        }
+    }
+
+
+
+
+    public static bool FindAndWriteValue(BinaryReader reader, BinaryWriter writer, byte[] propertyName, int writeValue)  // Writes value then returns if the value has been written to
+    {
+
+        int matchIndex = 0;
+        int skipSize = 4;
+
+        if (!StructFound)
+        {
+            LocateStructForBinaryReader(reader, Name_CurrencyStruct);
+        }
+
+        while (reader.BaseStream.Position < reader.BaseStream.Length && matchIndex < propertyName.Length)
+        {
+            byte b = reader.ReadByte();
+            if (b == propertyName[matchIndex])
+            {
+                matchIndex++;
+                if (matchIndex == propertyName.Length)
+                {
+                    reader.ReadBytes(skipSize); // Skip IntProperty Size
+                    reader.ReadBytes(IntProperty.Length);  // Skip IntProperty
+
+                    // Read data and handle potential issues
+                    try
+                    {
+                        reader.ReadBytes(skipSize);  // Skip Value Size
+                        reader.ReadBytes(skipSize);  // Skip Value ArrayIndex
+                        byte[] writeValueBytes = IntToLittleEndian(writeValue);
+                        writer.Write(writeValueBytes);
+                        matchIndex = 0; // Reset for next property search
+                        return true;
+
+                    }
+                    catch (EndOfStreamException)
+                    {
+                        // Handle end of stream gracefully (e.g., return default value)
+                        matchIndex = 0; // Reset for next property search
+                        return false; // Or throw an exception based on your application logic
+                    }
+
+                }
+            }
+            else
+            {
+                matchIndex = 0; // Reset match index if byte doesn't match
+            }
+        }
+
+        return false; // Property not found
+    }
+
+    public int FindAndReadValue(BinaryReader reader, byte[] propertyName)
+    {
+        int matchIndex = 0;
+        int skipSize = 4;
+
+        if (!StructFound)
+        {
+            LocateStructForBinaryReader(reader, Name_CurrencyStruct);
+        }
+
+        while (reader.BaseStream.Position < reader.BaseStream.Length && matchIndex < propertyName.Length)
+        {
+            byte b = reader.ReadByte();
+            if (b == propertyName[matchIndex])
+            {
+                matchIndex++;
+                if (matchIndex == propertyName.Length)
+                {
+                    reader.ReadBytes(skipSize); // Skip IntProperty Size
+                    reader.ReadBytes(IntProperty.Length);  // Skip IntProperty
+
+                    // Read data and handle potential issues
+                    try
+                    {
+                        reader.ReadBytes(skipSize);  // Skip Value Size
+                        reader.ReadBytes(skipSize);  // Skip Value ArrayIndex
+                        byte[] valueBytes = reader.ReadBytes(4);  // read value
+                        return LittleEndianToInt(valueBytes);
+                    }
+                    catch (EndOfStreamException)
+                    {
+                        // Handle end of stream gracefully (e.g., return default value)
+                        return -1; // Or throw an exception based on your application logic
+                    }
+                }
+            }
+            else
+            {
+                matchIndex = 0; // Reset match index if byte doesn't match
+            }
+        }
+
+        return -1; // Property not found
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Currency Struct Default Property
+
+Variable.IntProperty.Size.ArrayIndex.Value
+
+
+<>
+
+
+Current.IntProperty.4.0.MaxInt
+43 75 72 72 65 6E 74 00 0C 00 00 00 49 6E 74 50 72 6F 70 65 72 74 79 00 04 00 00 00 00 00 00 00 FF FF FF 7F
+<>
+Current
+43 75 72 72 65 6E 74 00
+IntPropSize
+0C 00 00 00
+IntProperty
+49 6E 74 50 72 6F 70 65 72 74 79 00
+Size
+04 00 00 00
+ArrayIndex
+00 00 00 00
+Value
+FF FF FF 7F
+Next properties name size
+0E 00 00 00
+
+*/
+
+/* Serialize
+using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+{
+    WriteProperty(writer, "Current", cs.Current);
+    WriteProperty(writer, "TotalAcquired", cs.TotalAcquired);
+    WriteProperty(writer, "TotalFoundInWorld", cs.TotalFoundInWorld);
+    WriteProperty(writer, "TotalFromBattle", cs.TotalFromBattle);
+}*/
